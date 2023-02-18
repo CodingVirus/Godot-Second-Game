087da03db3ba6984ccfc2a0aa37373128a2c0141
CodingVirus <glakffkdldk1@gmail.com>
2023-02-18 21:00:29 +0900

    section2 study

--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -1,11 +1,15 @@
 extends KinematicBody2D
 
-var maxSpeed = 100
+var maxSpeed = 25
 var velocity = Vector2.ZERO
 var direction = Vector2.RIGHT
+var gravity = 500
 
 func _process(delta):
 	velocity.x = (direction * maxSpeed).x	
+	
+	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 
+	
 	$AnimatedSprite.flip_h = true if direction.x > 0 else false
