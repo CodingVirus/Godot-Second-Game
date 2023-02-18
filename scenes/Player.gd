f5f75cd5d091829a75562465360bd939867c162e
CodingVirus <glakffkdldk1@gmail.com>
2023-02-18 16:07:51 +0900

    Section2 study

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -6,6 +6,7 @@ var maxHorizontalSpeed = 140
 var horizontalAcceleraton = 2000
 var jumpSpeed = 360
 var jumpTerminationMultiplier = 4
+var hasDoubleJump = false
 
 func _ready():
 	pass # Replace with function body.
@@ -18,18 +19,46 @@ func _process(delta):
 		
 	velocity.x = clamp(velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed)
 	
-	if (moveVector.y < 0 && is_on_floor()):
+	if (moveVector.y < 0 && (is_on_floor() || !$CoyoteTimer.is_stopped() || hasDoubleJump)):
 		velocity.y = moveVector.y * jumpSpeed
+		if (!is_on_floor() && $CoyoteTimer.is_stopped()):
+			hasDoubleJump = false
+		$CoyoteTimer.stop()
 	
 	if (velocity.y < 0 && !Input.is_action_pressed("jump")):
 		velocity.y += gravity * jumpTerminationMultiplier * delta
 	else:
 		velocity.y += gravity * delta
 	
+	var wasOnFloor = is_on_floor()
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	if (wasOnFloor && !is_on_floor()):
+		$CoyoteTimer.start()
+	
+	if (is_on_floor()):
+		hasDoubleJump = true
+		
+	update_animation()
 	
 func get_movement_vector():
 	var moveVector = Vector2.ZERO
 	moveVector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
 	return moveVector
+
+func update_animation():
+	var moveVec = get_movement_vector()
+	
+	if (!is_on_floor()):
+		$AnimatedSprite.play("jump")
+	elif (moveVec.x != 0):
+		$AnimatedSprite.play("run")
+	else:
+		$AnimatedSprite.play("idle")
+		
+	if (moveVec.x != 0):
+		$AnimatedSprite.flip_h = true if moveVec.x > 0 else false
+
+
+
