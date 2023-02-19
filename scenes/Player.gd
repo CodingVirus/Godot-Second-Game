3aba0d18b73ed69dd91ed5842643c3ba4afccf77
CodingVirus <glakffkdldk1@gmail.com>
2023-02-19 19:19:01 +0900

    section2 study

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -2,22 +2,40 @@ extends KinematicBody2D
 
 signal died
 
+enum State {NORMAL, DASHING}
+
 var gravity = 1000
 var velocity = Vector2.ZERO
 var maxHorizontalSpeed = 140
+var maxDashSpeed = 500
+var minDashSpeed = 200
 var horizontalAcceleraton = 2000
 var jumpSpeed = 360
 var jumpTerminationMultiplier = 4
 var hasDoubleJump = false
+var currentState = State.NORMAL
+var isStateNew = true
 
 func _ready():
 	$HazardArea.connect("area_entered", self, "on_hazard_area_entered")
 
 func _process(delta):
+	match currentState:
+		State.NORMAL:
+			process_normal(delta)
+		State.DASHING:
+			process_dash(delta)
+	isStateNew = false
+	
+func change_state(newState):
+	currentState = newState
+	isStateNew = true
+
+func process_normal(delta):
 	var moveVector = get_movement_vector()
 	velocity.x += moveVector.x * horizontalAcceleraton * delta
 	if (moveVector.x == 0):
-		velocity.x = lerp(0, velocity.x, pow(2, -50 * delta))
+		velocity.x = lerp(0, velocity.x, pow(2, -5 * delta))
 		
 	velocity.x = clamp(velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed)
 	
@@ -40,8 +58,21 @@ func _process(delta):
 	
 	if (is_on_floor()):
 		hasDoubleJump = true
+	
+	if (Input.is_action_just_pressed("dash")):
+		call_deferred("change_state", State.DASHING)
 		
 	update_animation()
+
+func process_dash(delta):
+	if (isStateNew):
+		velocity = Vector2(maxDashSpeed, 0)
+		
+	velocity = move_and_slide(velocity, Vector2.UP)
+	velocity.x = lerp(0, velocity.x, pow(2, -20 * delta))
+	
+	if (velocity.x < minDashSpeed):
+		call_deferred("change_state", State.NORMAL)
 	
 func get_movement_vector():
 	var moveVector = Vector2.ZERO
