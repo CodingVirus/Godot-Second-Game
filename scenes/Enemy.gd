245a1aa41791ef37cfb7b4f4e058a722c52feeee
CodingVirus <glakffkdldk1@gmail.com>
2023-02-19 17:45:15 +0900

    section2 study

--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -1,9 +1,16 @@
 extends KinematicBody2D
 
+enum Direction {RIGHT, LEFT}
+export(Direction) var startDirection
+
 var maxSpeed = 25
 var velocity = Vector2.ZERO
-var direction = Vector2.RIGHT
+var direction = Vector2.ZERO
 var gravity = 500
+
+func _ready():
+	direction = Vector2.RIGHT if startDirection == Direction.RIGHT else Vector2.LEFT
+	$GoalDetector.connect("area_entered", self, "on_goal_entered")
 
 func _process(delta):
 	velocity.x = (direction * maxSpeed).x	
@@ -13,3 +20,6 @@ func _process(delta):
 
 	
 	$AnimatedSprite.flip_h = true if direction.x > 0 else false
+	
+func on_goal_entered(_area2d):
+	direction *= -1
